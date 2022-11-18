09d7238e19fc49652092efaaf83f42a643d258fb
alexSiqueiraLp <alex0sql@outlook.com>
2022-11-18 18:13:09 -0300

    voltando para a alteracao de teste

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("rodando o sistema de gerenciamento de jogo")
 console.log("aplicação esta se conectando ao banco de dados")
 // mais um console de teste
 console.log("O paulo fez esse commit sem voce ver na aula!")
-console.log("Enviando para o github")
